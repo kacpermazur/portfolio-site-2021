5661fab0afc24d2abc0bee3e2eee779416f894bf
kacper <kacper.mazur888@gmail.com>
2021-10-15 04:45:24 +0100

    cards

--- js/card.js
+++ js/card.js
@@ -1,6 +1,7 @@
-const ROW                   = "projectCard-Row";
+const ROW                   = "row projectCard-Row";
 
 const CARD                  = "projectCard";
+const CARD_TITLE            = "card-title";
 const CARD_OVERLAY          = "projectCard-OverlayZone";
 const CARD_IMAGE            = "projectCard-Image";
 const CARD_TAGAREA          = "projectCard-TagArea";
@@ -14,6 +15,8 @@ const Logo = {"Unity":"https://upload.wikimedia.org/wikipedia/commons/thumb/1/19
 "OpenGL":"link", 
 "JavaScript":"link"};
 
+const CARD_STATE = {"CLOSED": 0, "OPENED": 1, "HOVER": 2};
+
 class Card
 {
     constructor()
@@ -26,6 +29,8 @@ class Card
         this.previewText = "This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.";
         this.fullText = "lol it works yay";
         this.linkButtons = ["link", "link"];
+        
+        this.state = CARD_STATE.CLOSED;
     }
 
     SetData(title, image, mainTag, tags, logo, previewText, fullText, linkButtons)
@@ -40,6 +45,11 @@ class Card
         this.linkButtons = linkButtons // Need
     }
 
+    GetState()  {    return this.state;}
+    Inspect()   {    this.state = CARD_STATE.HOVER;}
+    Open()      {    this.state = CARD_STATE.OPENED;}
+    Close()     {    this.state = CARD_STATE.CLOSED;}
+
     static ToHTML(Card)
     {
         const cardTemplate = `<div class="card text-white projectCard">
@@ -47,13 +57,19 @@ class Card
         <img class="bd-placeholder-img bd-placeholder-img-lg card-img projectCard-Image" src="${Card.image}" alt="bgImage">
   
         <div class="card-img-overlay projectCard-OverlayZone">
-          <!-- TITLE AND TECH LOGO -->
-          <h4 class="card-title">${Card.title} 
-            <img class="img-fluid float-end m-3" style="height: 35px;" src="${Card.logo}" alt="ProjectLogo">
-          </h4>
+            <div class="row justify-content-between">
+                <div class="col">
+                <h4 class="card-title">${Card.title}</h4>
 
-          <!-- SKILL TAGS -->
-          <h6 class="card-text projectCard-TagArea"></h6>
+                <!-- SKILL TAGS -->
+                <h6 class="card-text projectCard-TagArea"></h6>
+                </div>
+
+                <div class="col">
+                <!-- TITLE AND TECH LOGO -->
+                <img class="img-fluid float-end m-3" style="height: 40px;" src="${Card.logo}" alt="ProjectLogo">
+                </div>
+            </div>
 
           <!-- DESCRIPTION -->
           <p style="margin-top: 2.5%;" class="card-text text-blur-out projectCard-PreviewText">
@@ -86,10 +102,71 @@ class Card
     }
 }
 
+class CardFactory
+{
+    static projectCards = new Map();
+    static activeProjectCard = null
+    static projectCardCount  = 0;
+    static projectRowCount   = -1;
+
+    static Add(card)
+    {
+        if(this.projectCards.has(card.title))
+        {
+            console.log(`Unable to add card with same project title! --> ${card.title}`);
+            return;
+        }
+
+        const projectCardArea = document.getElementsByClassName("projectCard-Container")[0];
+
+        if(this.projectCardCount % 2 == 0)
+        {
+            const nRow = document.createElement('div');
+            nRow.className = ROW;
+            projectCardArea.appendChild(nRow);
+            this.projectRowCount++;
+            
+        }
+        
+        const currentRow = document.getElementsByClassName(ROW)[this.projectRowCount];
+        const tempCard = Card.ToHTML(card);
+
+        currentRow.appendChild(tempCard);
+        this.#RegisterEvents(tempCard);
+
+        this.projectCards.set(card.title, card);
+        this.projectCardCount++;
+        console.log(`Card: ${Card.title} has been added!`);
+    }
+
+    static #RegisterEvents(cardHTML)
+    {   
+        const overLay = cardHTML.getElementsByClassName(CARD_OVERLAY)[0];
+        overLay.addEventListener('mouseenter', this.#OnHover);
+        overLay.addEventListener('mouseleave', test);
+    }
+
+    static #OnHover(e)
+    {
+        console.log(e.target.getElementsByClassName(CARD_TITLE)[0].innerHTML);
+    }
+
+    static GetActiveCard()
+    {
+        return this.activeProjectCard;
+    }
+
+    static LogCards()
+    {
+        console.log(`Card Count: ${this.projectCardCount} | Row Count: ${this.projectRowCount}`);
+    }
+}
 
 
-
-
+function test(e)
+{
+    console.log(`fwefwsadasdefwe`);
+}
 
 
 
@@ -107,9 +184,9 @@ else
 
 function Start()
 {
-    console.log("Loaded");
+    let cardA = new Card();
 
-    let testCard = new Card();
-
-    document.getElementsByClassName(ROW)[0].appendChild(Card.ToHTML(testCard));
+    CardFactory.Add(cardA);
+    CardFactory.LogCards();
 }
+
